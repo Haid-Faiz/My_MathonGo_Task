c68e11a3af0f924c0529faacd5eb230711b1bc77
Faizan Haider <haiderfaizan732@gmail.com>
2021-12-06 12:21:23 +0530

    refactor: Adds release build apk & refactors code

--- datasource/src/main/java/com/example/datasource/remote/apis/MathonGoApi.kt
+++ datasource/src/main/java/com/example/datasource/remote/apis/MathonGoApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface MathonGoApi {
 
-    @GET("JEE_Main_-_Gravitation.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211205%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211205T052322Z&X-Amz-Expires=86400&X-Amz-Signature=89a172313007503854d239bbb425b9e792921471c04ae543b3d80018f48c2fec&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22JEE%2520Main%2520-%2520Gravitation.json%22&x-id=GetObject")
+    @GET("JEE_Main_-_Gravitation.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211206%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211206T060656Z&X-Amz-Expires=86400&X-Amz-Signature=6378aa5256624e23863870d06188185b3e2dda26545a89d8be1564c6378f75ca&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22JEE%2520Main%2520-%2520Gravitation.json%22&x-id=GetObject")
     suspend fun fetchQuestionsList(): Response<List<QuestionItem>>
 }
